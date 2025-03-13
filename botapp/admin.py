cfaab59ebfd21bc4d6f791846c5b2c3ecb8fac05
Ulugbek Abdulazizov <aqllibola567@gmail.com>
2025-03-13 18:00:14 +0500

    created model for botusers and register

--- botapp/admin.py
+++ botapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import BotUser
 
-# Register your models here.
+
+class BotUserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'phone_number', 'username', 'first_name', 'last_name', 'is_active', 'is_admin')
+    list_filter = ('is_active', 'is_admin')
+    search_fields = ('phone_number', 'username', 'first_name', 'last_name')
+
+
+admin.site.register(BotUser, BotUserAdmin)
--- botapp/models.py
+++ botapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+# Botga foydalanuvchilarini saqlash uchun model
+class BotUser(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    phone_number = models.CharField(max_length=15)
+    username = models.CharField(max_length=255, null=True, blank=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
