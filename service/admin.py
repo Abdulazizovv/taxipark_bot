640fefd86f5cbdf98ef5751690b566ac589bebeb
Ulugbek Abdulazizov <aqllibola567@gmail.com>
2025-03-13 17:07:46 +0500

    created model for services and registering to admin panel

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Service
 
-# Register your models here.
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['title', 'phone_number', 'description']
+    search_fields = ['title', 'phone_number']
+    list_filter = ['category']
+    list_per_page = 10
+
+# admin panelga qo'shish
+admin.site.register(Service, ServiceAdmin)
--- service/models.py
+++ service/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from service_category.models import ServiceCategory
 
-# Create your models here.
+
+# Servislar uchun model
+class Service(models.Model):
+    phone_number = models.CharField(max_length=20)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    category = models.ManyToManyField(ServiceCategory, related_name="services")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Servis"
+        verbose_name_plural = "Servislar"
+
+    def __str__(self):
+        return self.title
