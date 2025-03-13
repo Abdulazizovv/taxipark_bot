da610bdf0b4dfb36cabbc1cc90ad8142be27dda7
Ulugbek Abdulazizov <aqllibola567@gmail.com>
2025-03-13 16:16:34 +0500

    created model for drivers and register to django admin panel

--- driver/admin.py
+++ driver/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Driver
 
-# Register your models here.
+
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'phone_number', 'car_model', 'car_color', 'car_plate', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('is_active', 'created_at', 'updated_at')
+    search_fields = ('full_name', 'phone_number', 'car_model', 'car_color', 'car_plate')
+    list_per_page = 25
+
+# django admin panelga driver modelini qo'shish
+admin.site.register(Driver, DriverAdmin)
--- driver/models.py
+++ driver/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+# Haydovchilar modeli
+class Driver(models.Model):
+    phone_number = models.CharField(max_length=15, unique=True)
+    full_name = models.CharField(max_length=100)
+    car_model = models.CharField(max_length=100)
+    car_color = models.CharField(max_length=100)
+    car_plate = models.CharField(max_length=10, unique=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.full_name
