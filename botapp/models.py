747665d968487d350d7935594f34a106d7a415da
Ulugbek Abdulazizov <aqllibola567@gmail.com>
2025-03-14 09:43:44 +0500

    feat: add role validation for users

--- botapp/models.py
+++ botapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 # Botga foydalanuvchilarini saqlash uchun model
@@ -11,6 +12,21 @@ class BotUser(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_admin = models.BooleanField(default=False)
+    is_service = models.BooleanField(default=False)
+
+    # Foydalanuvchini admin yoki servisdan biri ekanligini tekshirish
+    def clean(self):
+        if self.is_admin and self.is_service:
+            raise ValidationError("A user cannot be both an admin and a service user.")
+    
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'Bot foydalanuvchisi'
+        verbose_name_plural = 'Bot foydalanuvchilari'
+        ordering = ['-created_at']
