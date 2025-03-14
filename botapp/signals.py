bd41d59acb10aea74e7a7d8bca939227cb3936e4
Ulugbek Abdulazizov <aqllibola567@gmail.com>
2025-03-14 09:31:39 +0500

    created signals for botuser

---
+++ botapp/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import BotUser
+from manager.models import Manager
+
+
+@receiver(post_save, sender=BotUser)
+def check_user_is_admin(sender, instance, created, **kwargs):
+    if created:
+        if instance.phone_number in Manager.objects.values_list("phone_number", flat=True):
+            instance.is_admin = True
+            instance.save()
+        
+
+        
